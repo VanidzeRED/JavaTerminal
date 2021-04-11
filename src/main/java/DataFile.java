cd99825db6ebcbdfbf4aae867ef3215cabd87b17
Ivan Pomelnikov <vanidzepomelnikov@gmail.com>
2021-04-11 14:14:44 +0400

    reading from file to terminal

--- src/main/java/DataFile.java
+++ src/main/java/DataFile.java
@@ -5,6 +5,15 @@ public class DataFile {
     String fileName;
     FileWriter fileWriter;
     FileReader fileReader;
+    int strLen;
+
+    public void setStrLen(int strLen) {
+        this.strLen = strLen;
+    }
+
+    public int getStrLen() {
+        return strLen;
+    }
 
     public DataFile (String fileName){
         this.fileName = fileName;
@@ -20,16 +29,23 @@ public class DataFile {
         try {
             fileWriter.append(string);
             fileWriter.flush();
+            setStrLen(string.length());
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-/*
-    public String  readFromFile () {
-        try {
 
+    public String  readFromFile () {
+        char[] data = new char[getStrLen()];
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            fileReader.read(data);
+            for (char currentByte : data) {
+                stringBuilder.append(currentByte);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }*/
+        return stringBuilder.toString();
+    }
 }
